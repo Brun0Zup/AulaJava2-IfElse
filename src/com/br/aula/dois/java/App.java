68b2d6f62cc38feb38924e0975f66ed4ce92ac60
Bruno <Bruno.cardoso@zup.com.br>
2019-05-18 00:22:00 -0300

    Adicionando o if e else duas estruturas condicionais

---
+++ src/com/br/aula/dois/java/App.java
@@ -0,0 +1,24 @@
+package com.br.aula.dois.java;
+
+public class App {
+
+	public static void main(String[] args) {
+	 
+		// & - e
+		// | - ou
+		
+		int idade = 25;
+  boolean cartaDeMotorista = true;
+  
+  if (idade >= 18 && cartaDeMotorista == true) {
+	  System.out.println("pode dirigir");
+  } else {
+	  
+	  System.out.println("Não ode dirigir");
+  }
+  
+  
+			
+	}
+
+}
